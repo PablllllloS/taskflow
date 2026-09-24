0a1a3c15ba85eae462bee3b18ac81fdba25bcd41
Pablo <pablo.a.lopes@aluno.senai.br>
2026-09-24 10:53:14 -0300

    .env configurado

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://taskflow-lbmnujf3x-sky-48d0.vercel.app/' });
+const api = axios.create({ baseURL: import.meta.env.VITE_BASEURL });
 
 api.interceptors.request.use(config => {
     const token = localStorage.getItem('token');
